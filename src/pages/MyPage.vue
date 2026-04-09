650983251215bc76bbac1d111cbecd7280545afb
sanchez <kastor0323@gmail.com>
2026-04-09 11:48:36 +0900

    마이페이지 개발 시작

--- src/pages/MyPage.vue
+++ src/pages/MyPage.vue
@@ -1,2 +1,12 @@
 // 마이페이지 (사용자 정보)
-<template>마이페이지</template>
+<template>
+  <header class="topbar">
+    <nav class="topbar-nav">
+      <router-link class="topbar-btn ledgers" to="/ledgers">
+        캘린더로 돌아가기</router-link
+      >
+    </nav>
+  </header>
+</template>
+
+<script></script>
