ba80df3cf04ebcda939c321b190d6ead8be76be7
sanchez <kastor0323@gmail.com>
2026-04-09 11:15:01 +0900

    Feat_AppHeader and LoginForm and styles
    Add AppHeader logic, icon and style
    Add LoginFrom icon and style
    Refactor theme.css -> login.css and appHeader.css

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@ authStore.initializeAuth();
 </script>
 
 <template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+  />
   <RouterView />
 </template>
 
--- src/pages/AnalysisPage.vue
+++ src/pages/AnalysisPage.vue
@@ -1,1 +1,9 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue';
+</script>
+
 // 소비 분석 화면 (그래프 + 분석 결과)
+<template>
+  <AppHeader></AppHeader>
+  analysis
+</template>
